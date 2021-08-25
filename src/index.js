383f1cb10aaa6ab3c0d26223e0edef1269aaf3d8
JSONRete <jsonrete@gmail.com>
2021-08-25 13:00:29 -0400

    test rendering 'info' as a list

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@
 // .then(res => res.json())
 // .then(data => console.log(data))
 
+const list =  document.getElementById("info-container")
+
 fetch("http://127.0.0.1:3000/infos")
 .then(res => res.json())
 .then(renderInfos)
 
 function renderInfos(tet){
-    debugger
-}
+    const infos = tet["data"]
+    liElements = infos.map(function(info) {
+        let li = document.createElement('li')
+        li.innerText = `Song: ${info.attributes.name} - BPM: ${info.attributes.tempo} - KEY: ${info.attributes.key}`
+        return li
+    })
+
+    liElements.forEach(element => {
+        list.appendChild(element)
+    });
+}  
