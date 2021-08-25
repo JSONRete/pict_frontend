044fce68f7175b0bfaa51fa34f019fde252f7904
JSONRete <jsonrete@gmail.com>
2021-08-25 10:49:39 -0400

    created separation of concerns directories & files  with 'adapters' & 'components'

---
+++ src/adapters/InfosAdapter.js
@@ -0,0 +1,12 @@
+class InfoAdapter {
+    constructor() {
+        this.baseUrl = "http://127.0.0.1:3000/infos"
+    }
+
+
+
+    getInfo() {
+        return fetch(this.baseUrl).then(res => res.json()
+        )
+    }
+}
