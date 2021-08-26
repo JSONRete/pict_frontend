26598aad2b24f5f9f15037e3521cbe95fd46e353
JSONRete <jsonrete@gmail.com>
2021-08-26 08:15:17 -0400

    updated entry form

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ fetch("http://127.0.0.1:3000/infos")
 .then(renderInfos)
 
 function renderInfos(tet){
-    const infos = tet["data"]
-    liElements = infos.map(function(info) {
-        let li = document.createElement('li')
+    const infos = tet.data
+    const liElements = infos.map(function(info) {
+        const li = document.createElement('li')
         li.innerText = `Song: ${info.attributes.name} - BPM: ${info.attributes.tempo} - KEY: ${info.attributes.key}`
         return li
     })
