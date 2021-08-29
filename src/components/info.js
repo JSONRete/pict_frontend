cf14560d6a01e2481211ddb54b97d661798022c1
JSONRete <jsonrete@gmail.com>
2021-08-29 14:37:33 -0400

    updated class Info {} constructor

--- src/components/info.js
+++ src/components/info.js
@@ -1,10 +1,19 @@
-class Info {
-    constructor(infoJSON) {
-        this.id = infoJSON.id
-        this.name = infoJSON.name
-        this.tempo = infoJSON.tempo
-        this.key = infoJSON.key
-        this.toolId = infoJSON.tool_id
-    }
+// class Info {
+//     constructor(infoJSON) {
+//         this.id = infoJSON.id
+//         this.name = infoJSON.name
+//         this.tempo = infoJSON.tempo
+//         this.key = infoJSON.key
+//         this.toolId = infoJSON.tool_id
+//     }
+// }
 
+class Info {
+    constructor({id, name, tempo, key, tool_id}) {
+        this.id = id
+        this.name = name
+        this.tempo = tempo
+        this.key = key
+    
+    }
 }
